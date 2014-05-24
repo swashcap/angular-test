093ac856be404ea683d942005c93197094abac1a
Swashcap <swashcap@gmail.com>
2014-05-24 11:45:47 -0700

    Integrate article service in controller and view.

--- app/scripts/controllers/articles.js
+++ app/scripts/controllers/articles.js
@@ -1,10 +1,21 @@
 'use strict';
 
 angular.module('angularTestApp')
-  .controller('ArticlesCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('ArticlesCtrl', ['article', '$scope', function (article, $scope) {
+    // Get articles, add to scope
+    article.get()
+      .then(function (articles) {
+        $scope.articles = articles.data;
+        //$scope.articles = articles;
+        //$scope.$$phase || $scope.$apply();
+        //$rootScope.$$phase || $rootScope.$apply();
+        //$scope.$apply(function () {});//articles = articles;
+      })
+      .catch(function (data) {
+        console.log('Could not get articles', data);
+      });
+
+
+    //console.log(articles);
+    //$scope.articles = article.get();
+  }]);
