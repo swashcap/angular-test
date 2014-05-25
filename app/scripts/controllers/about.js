d7948852f54f13e8d0ca8add1dc3fd80ac1e0c1b
Swashcap <swashcap@gmail.com>
2014-05-25 11:42:44 -0700

    Add 'About' page clients list feature.

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -1,10 +1,60 @@
 'use strict';
 
 angular.module('angularTestApp')
-  .controller('AboutCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('AboutCtrl', ['$scope', function ($scope) {
+    $scope.clients = [{
+      name: 'Google',
+      logo: '/images/logo-1.jpg'
+    }, {
+      name: 'Twitter',
+      logo: '/images/logo-2.jpg'
+    }, {
+      name: 'Target',
+      logo: '/images/logo-3.jpg'
+    }, {
+      name: 'Coca Cola',
+      logo: '/images/logo-1.jpg'
+    }, {
+      name: 'Apple',
+      logo: '/images/logo-2.jpg'
+    }, {
+      name: 'Starbucks',
+      logo: '/images/logo-3.jpg'
+    }, {
+      name: 'Ford',
+      logo: '/images/logo-1.jpg'
+    }, {
+      name: 'NBA',
+      logo: '/images/logo-2.jpg'
+    }, {
+      name: 'Nike',
+      logo: '/images/logo-3.jpg'
+    }, {
+      name: 'AirBnB',
+      logo: '/images/logo-1.jpg'
+    }, {
+      name: 'Route 66',
+      logo: '/images/logo-2.jpg'
+    }, {
+      name: 'Macys',
+      logo: '/images/logo-3.jpg'
+    }, {
+      name: 'Nestle',
+      logo: '/images/logo-1.jpg'
+    }, {
+      name: 'Ikea',
+      logo: '/images/logo-2.jpg'
+    }, {
+      name: 'Boeing',
+      logo: '/images/logo-3.jpg'
+    }, {
+      name: 'Apple Records',
+      logo: '/images/logo-1.jpg'
+    }, {
+      name: 'Safeway',
+      logo: '/images/logo-2.jpg'
+    }, {
+      name: 'You!',
+      logo: '/images/logo-3.jpg'
+    }];
+  }]);
